5fc6a5fc231cffd3079e1d4846233e0068b3a947
shoryasoni21 <136825626+shoryasoni21@users.noreply.github.com>
2023-06-23 19:39:12 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,37 @@
+
+var trex ,trex_running;
+function preload(){
+  trex_running=loadAnimation("trex1.png","trex3.png","trex4.png");
+
+}
+
+function setup(){
+  createCanvas(600,200);
+  
+  //create a trex sprite
+  trex=createSprite(50,160,20,50);
+  trex.addAnimation("running",trex_running);
+
+//adding scale and position yo trex
+trex.scale=0.5;
+trex.x=50
+
+//create a ground sprite
+ground=createSprite(200,180,400,20);
+ 
+}
+
+function draw(){
+  background("white");
+  
+  if(keyDown("space"))
+  {
+trex.velocityY=-10;   
+
+  }
+  //adding gravity
+  trex.velocityY=trex.velocityY+0.5;
+//stop trex from falling down
+trex.collide(ground);
+drawSprites();
+}
